658978e2c5882cf21f2efe0009f5c22c248328a7
David Ayers <dacoayers@yahoo.com>
2023-02-15 00:47:24 -0500

    v1

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
 'use strict';
 
-console.log('Hello'); 
+//List Employee
+const emlployees = [];
+
+//Employee Title
+const empTitle = [
+   ' Captain',
+    'Sergeant',
+    'corporal',
+    'Police Officer',
+    'Dispatcher'
+]; 
+
+// Employee Call Sign
+const callSigns = []; 
+
+//8Hr Days
+const shortDaySunday =[];
+const shortDayMonday =[];
+const shortDayTuesday =[];
+const shortDayWednesday =[];
+const shortDayThursday =[];
+const shortDayFriday =[];
+const shortDaySaturday =[];
+
+// Training Days Off
+const trainingDayOffSunday =[];
+const trainingDayOffMonday =[];
+const trainingDayOffTuesday =[];
+const trainingDayOffWednesday =[];
+const trainingDayOffThursday =[];
+const trainingDayOffFriday =[];
+const trainingDayOffSaturday =[];
+
+//Time Keeping
+const AnuallLeave = [];
+const sickLeave = []; 
+const leavePther =[];
+
+//Post Assignment 
+const post = [];
+//Time Rotations
+const time = []; 
+
+
+
+
+shortDays.Sunday.push("Ayers");
+
+emlployees.push("Ayers", 'Watson'); 
+
+console.log(shortDays);
+
+console.log(...emlployees);
+
+console.log(typeof(shortDays.Sunday)); 
